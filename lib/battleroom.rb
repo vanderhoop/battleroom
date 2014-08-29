d71c779a0041dd4c981106fade60d7b9eb4fe785
vanderhoop <vanderhoop@me.com>
2014-08-28 23:23:08 -0400

    allows user to exit from prompts

--- lib/battleroom.rb
+++ lib/battleroom.rb
@@ -21,36 +21,37 @@ while true
     end
   when "2"
     5.times do
-      question = DATA_STRUCTURE_QUESTIONS.sample
-      data_structure = question[:data_structure]
-      if data_structure.class == Array
-        answer_value = data_structure.sample
-        hint = "index values start at 0."
-      else
-        answer_value = data_structure[data_structure.keys.sample]
-        hint = "you have to use the EXACT hash key to retrieve the associated value."
-      end
-      answer_value = data_structure.class == Array ? data_structure.sample : data_structure[data_structure.keys.sample]
-      # provides the binding scope with the variable assignment necessary for future
-      b.eval("#{question[:variable_name]} = #{question[:data_structure].to_s}")
-      puts "Given the data structure below, how would you access '#{answer_value}'?".blue
-      puts  "#{question[:variable_name]} = #{question[:data_structure].to_s}".green
-      answered_correctly = false
-      until answered_correctly
-        input = gets.chomp
-        begin
-          if b.eval(input) == answer_value
-            print_congratulation
-            answered_correctly = true
-            sleep 1.5
-            clear_display
-          else
-            puts "Wrong. Remember, #{hint} Try again.".red
-          end
-        rescue NameError
-          print_colorized_name_error_prompt
-        end
-      end
+      evaluate_data_structure_access_response(b)
+      # question = DATA_STRUCTURE_QUESTIONS.sample
+      # data_structure = question[:data_structure]
+      # if data_structure.class == Array
+      #   answer_value = data_structure.sample
+      #   hint = "index values start at 0."
+      # else
+      #   answer_value = data_structure[data_structure.keys.sample]
+      #   hint = "you have to use the EXACT hash key to retrieve the associated value."
+      # end
+      # answer_value = data_structure.class == Array ? data_structure.sample : data_structure[data_structure.keys.sample]
+      # # provides the binding scope with the variable assignment necessary for future
+      # b.eval("#{question[:variable_name]} = #{question[:data_structure].to_s}")
+      # puts "Given the data structure below, how would you access '#{answer_value}'?".blue
+      # puts  "#{question[:variable_name]} = #{question[:data_structure].to_s}".green
+      # answered_correctly = false
+      # until answered_correctly
+      #   input = gets.chomp
+      #   begin
+      #     if b.eval(input) == answer_value
+      #       print_congratulation
+      #       answered_correctly = true
+      #       sleep 1.5
+      #       clear_display
+      #     else
+      #       puts "Wrong. Remember, #{hint} Try again.".red
+      #     end
+      #   rescue NameError
+      #     print_colorized_name_error_prompt
+      #   end
+      # end
     end
   when /^(q|exit\s?)/i
     puts "Goodbye!".green
--- lib/battleroom/battleroom_machinery.rb
+++ lib/battleroom/battleroom_machinery.rb
@@ -41,6 +41,7 @@ module BattleroomMachinery
     answered_correctly = false
     until answered_correctly
       answer = gets.chomp
+      abort("Goodbye!".green) if answer.match /^exit\s?/i
       begin
         evaluation_scope.eval(answer)
         if evaluation_scope.eval("#{@var_name} == #{@var_value}")
@@ -50,9 +51,51 @@ module BattleroomMachinery
           print "You mis-assigned #{var_name}. ".red + "Try Again!\n".green
         end
       rescue NameError
-        print_colorized_name_error_prompt
+        puts "Looks like you mistyped the variable name. Check for misspellings and try again.".red
       rescue Exception => e
         puts e.message
+      end
+    end
+  end
+
+  def format_question_hash_based_on_data_structure_class(randomly_assigned_question)
+    data_structure = randomly_assigned_question[:data_structure]
+    if data_structure.class == Array
+      randomly_assigned_question[:answer_value] = data_structure.sample
+      randomly_assigned_question[:hint] = "index values start at 0."
+    else
+      randomly_assigned_question[:answer_value] = data_structure[data_structure.keys.sample]
+      randomly_assigned_question[:hint] = "you have to use the EXACT hash key to retrieve the associated value."
+    end
+    randomly_assigned_question
+  end
+
+  def print_data_structure_access_prompt(given_question)
+    puts "Given the data structure below, how would you access '#{given_question[:answer_value]}'?".blue
+    puts "#{given_question[:variable_name]} = #{given_question[:data_structure].to_s}".green
+  end
+
+  def evaluate_data_structure_access_response(evaluation_scope)
+    question_hash = DATA_STRUCTURE_QUESTIONS.sample
+    question_hash = format_question_hash_based_on_data_structure_class(question_hash)
+    # provides the binding scope with the variable assignment necessary for future
+    evaluation_scope.eval("#{question_hash[:variable_name]} = #{question_hash[:data_structure].to_s}")
+    print_data_structure_access_prompt(question_hash)
+    answered_correctly = false
+    until answered_correctly
+      input = gets.chomp
+      break if input === /^exit\s?/i
+      begin
+        if evaluation_scope.eval(input) == question_hash[:answer_value]
+          print_congratulation
+          answered_correctly = true
+          sleep 1.5
+          clear_display
+        else
+          puts "Wrong. Remember, #{question[:hint]} Try again.".red
+        end
+      rescue NameError
+        print_colorized_name_error_prompt
       end
     end
   end
