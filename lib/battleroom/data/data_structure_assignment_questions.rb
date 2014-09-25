46426fccbd93a226387b870f99a8c15898ef01e1
vanderhoop <vanderhoop@me.com>
2014-09-25 10:04:13 -0400

    pulls data_generation code into module

--- lib/battleroom/data/data_structure_assignment_questions.rb
+++ lib/battleroom/data/data_structure_assignment_questions.rb
@@ -1,22 +1,6 @@
 require 'active_support/inflector'
-
-def gen_phone_number
-  "#{rand(1..9)}
-   #{rand(0..9)}
-   #{rand(0..9)}-
-   #{rand(0..9)}
-   #{rand(0..9)}
-   #{rand(0..9)}-
-   #{rand(0..9)}
-   #{rand(0..9)}
-   #{rand(0..9)}
-   #{rand(0..9)}".gsub(" ", "").gsub("\n", "")
-end
-
-def gen_password
-  possible_chars = ('A'..'Z').to_a + ('a'..'z').to_a + (0..9).to_a.map(&:to_s)
-  possible_chars.shuffle[0, rand(6..8)].join
-end
+require_relative '../data_generation_machinery'
+include DataGenerationMachinery
 
 DATA_STRUCTURE_ASSIGNMENT_QUESTIONS = [
   {
@@ -69,83 +53,18 @@ DATA_STRUCTURE_ASSIGNMENT_QUESTIONS = [
       "paperback_titles"
     ].shuffle,
   },
-].shuffle
+]
 
 6.times do
-  first_name = Faker::Name::first_name
-  last_name = Faker::Name::last_name
-  num_string = "#{[rand(0..9), rand(0..9), rand(0..9)][0, rand(1..4)].join}"
-  user = {
-    name: first_name + " " + last_name,
-    username: Faker::Internet::user_name(first_name) + num_string,
-    password: gen_password,
-    email: Faker::Internet.free_email(first_name),
-    is_admin: [true, false].sample,
-    phone: gen_phone_number,
-    accepts_marketing_emails: [true, false].sample,
-    accepts_promotional_emails: [true, false].sample,
-    occupation: Faker::Name.title,
-    zip: Faker::Address.zip
-  }
-
-  data = { data_structure: user,
-    possible_variable_names: [
-      first_name.downcase
-    ]
-  }
-  DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.push(data)
+  user = gen_user
+  DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.push(user)
+  business = gen_business
+  DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.push(business)
 end
 
-6.times do
-  name = Faker::Company.name
-  business = {
-    name: name,
-    public_email: "#{["info", "hello", "greetings", "contact"].sample}@#{name.parameterize}.com",
-    website: Faker::Internet.url("#{name.parameterize}.com", ''),
-    phone_num: gen_phone_number,
-    street_address: Faker::Address.street_address,
-    city: Faker::Address.city,
-    state: Faker::Address.state_abbr,
-    llc: [true, false].sample,
-    established: rand(1901..2014),
-  }
-  data = {
-    data_structure: business,
-    possible_variable_names: [
-      name.downcase.gsub(",", "").gsub(" ", "_").underscore
-    ]
-  }
-  DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.push(data)
-end
 4.times do
-  which_key_to_use = [:short, :long].sample
-  latitude  = { :short => :lat, :long => :latitude }
-  longitude = { :short => :long, :long => :longitude }
-  city = Faker::Address.city
-  population = rand(130..270000)
-  incorporated = population > 400 ? true : false
-  location = {
-    city: city,
-    population: population,
-    incorporated: incorporated,
-    livestock: [true, false].sample,
-    latitude[which_key_to_use] => Faker::Address.latitude.slice(0, rand(7..9)).to_f,
-    longitude[which_key_to_use] => Faker::Address.longitude.slice(0, rand(7..9)).to_f
-  }
-  data = {
-    data_structure: location,
-    possible_variable_names: [
-      city.downcase.gsub(",", "").gsub(" ", "_").underscore,
-      "home",
-      "target",
-      "destination",
-      "origin",
-      "locale",
-      "precinct",
-      "site",
-    ].shuffle
-  }
-  DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.push(data)
+  location = gen_location
+  DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.push(location)
 end
 
 DATA_STRUCTURE_ASSIGNMENT_QUESTIONS.shuffle!
---
+++ lib/battleroom/data_generation_machinery.rb
@@ -0,0 +1,98 @@
+
+module DataGenerationMachinery
+  def gen_phone_number
+    "#{rand(1..9)}
+     #{rand(0..9)}
+     #{rand(0..9)}-
+     #{rand(0..9)}
+     #{rand(0..9)}
+     #{rand(0..9)}-
+     #{rand(0..9)}
+     #{rand(0..9)}
+     #{rand(0..9)}
+     #{rand(0..9)}".gsub(" ", "").gsub("\n", "")
+  end
+
+  def gen_password
+    possible_chars = ('A'..'Z').to_a + ('a'..'z').to_a + (0..9).to_a.map(&:to_s)
+    possible_chars.shuffle[0, rand(6..8)].join
+  end
+
+  def gen_user
+    first_name = Faker::Name::first_name
+    last_name = Faker::Name::last_name
+    num_string = "#{[rand(0..9), rand(0..9), rand(0..9)][0, rand(1..4)].join}"
+    user = {
+      name: first_name + " " + last_name,
+      username: Faker::Internet::user_name(first_name) + num_string,
+      password: gen_password,
+      email: Faker::Internet.free_email(first_name),
+      is_admin: [true, false].sample,
+      phone: gen_phone_number,
+      accepts_marketing_emails: [true, false].sample,
+      accepts_promotional_emails: [true, false].sample,
+      occupation: Faker::Name.title,
+      zip: Faker::Address.zip
+    }
+
+    data = {
+      data_structure: user,
+      possible_variable_names: [
+        first_name.downcase
+      ]
+    }
+  end
+
+  def gen_business
+    name = Faker::Company.name
+    business = {
+      name: name,
+      public_email: "#{["info", "hello", "greetings", "contact"].sample}@#{name.parameterize}.com",
+      website: Faker::Internet.url("#{name.parameterize}.com", ''),
+      phone_num: gen_phone_number,
+      street_address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state_abbr,
+      llc: [true, false].sample,
+      established: rand(1901..2014),
+    }
+    data = {
+      data_structure: business,
+      possible_variable_names: [
+        name.downcase.gsub(",", "").gsub(" ", "_").underscore
+      ]
+    }
+  end
+
+  def gen_location
+    which_key_to_use = [:short, :long].sample
+    latitude  = { :short => :lat, :long => :latitude }
+    longitude = { :short => :long, :long => :longitude }
+    city = Faker::Address.city
+    population = rand(130..270000)
+    incorporated = population > 400 ? true : false
+    location = {
+      city: city,
+      population: population,
+      incorporated: incorporated,
+      livestock: [true, false].sample,
+      latitude[which_key_to_use] => Faker::Address.latitude.slice(0, rand(7..9)).to_f,
+      longitude[which_key_to_use] => Faker::Address.longitude.slice(0, rand(7..9)).to_f
+    }
+    data = {
+      data_structure: location,
+      possible_variable_names: [
+        city.downcase.gsub(",", "").gsub(" ", "_").underscore,
+        "home",
+        "target",
+        "destination",
+        "origin",
+        "locale",
+        "precinct",
+        "site",
+      ].shuffle
+    }
+  end
+end
+
+
