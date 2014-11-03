2b97c0ead765c6f6033c70ade0065e3e95bf9f38
vanderhoop <vanderhoop@me.com>
2014-11-03 14:45:23 -0800

    further amends language in MethodDefinitionFollowUpQuestion prompt

--- lib/battleroom/models/method_definition_follow_up_question.rb
+++ lib/battleroom/models/method_definition_follow_up_question.rb
@@ -9,21 +9,26 @@ class MethodDefinitionFollowUpQuestion < Question
     @original_question = question_to_follow_up_on
   end
 
-  def print_method_invocation_prompt
+  def format_method_definition_for_stdout
     code = CodeRay.scan($input, :ruby)
+    code.term.gsub(/^.*/) {|match| "\t" + match }
+  end
+
+  def print_method_invocation_prompt
     desired_answer_formatted = format_value_for_stdout_and_eval(original_question.eval_answer)
     desired_answer_class_formatted = format_class_for_output(original_question.eval_answer.class)
-    puts "You now have the method below at your disposal.\n".blue
+    puts "You now have the method defined below at your disposal.\n".blue
 
-    puts code.term + "\n"
+    puts format_method_definition_for_stdout
 
-    puts "Call it such that it returns the ".blue + desired_answer_class_formatted.blue + " value ".blue + desired_answer_formatted.yellow + "\n\n"
+    puts "\nCall ".blue + original_question.method_name.yellow + " such that it returns the ".blue + desired_answer_class_formatted.blue + " value ".blue + desired_answer_formatted.yellow + "\n\n"
   end
 
   def evaluate_user_input
     enter_evaluation_loop do |user_input|
       return_from_eval = original_question.evaluation_scope.eval(user_input)
       if return_from_eval == original_question.eval_answer
+        puts ''
         true
       else
         puts "Remember, to call a method, you simply enter its name followed by any arguments it might need. Try again.".red
