da6498ebaf974dbdd65ca3ae525d9f55e999f745
vanderhoop <vanderhoop@me.com>
2014-11-23 10:29:54 -0500

    adds tests for variable question data

--- spec/data_specs/variable_assignment_questions_spec.rb
+++ spec/data_specs/variable_assignment_questions_spec.rb
@@ -4,4 +4,30 @@ describe "VARIABLE_QUESTIONS" do
   it "is an array" do
     expect(VARIABLE_QUESTIONS.class).to eq(Array)
   end
+
+  it "has at least 5 values" do
+    expect(VARIABLE_QUESTIONS.size).to be >= 5
+  end
+
+  describe "each Variable Question" do
+    it "is stored as a hash" do
+      VARIABLE_QUESTIONS.each do |variable_question|
+        expect(variable_question.class).to eq(Hash)
+      end
+    end
+
+    it "has an array of possible_variable_names" do
+      VARIABLE_QUESTIONS.each do |variable_question|
+        expect(variable_question[:possible_variable_names].class).to eq(Array)
+      end
+    end
+
+    describe "each array of possible_variable_names" do
+      it "has contents" do
+        VARIABLE_QUESTIONS.each do |variable_question|
+          expect(variable_question[:possible_variable_names].size).to be >= 1
+        end
+      end
+    end
+  end
 end
