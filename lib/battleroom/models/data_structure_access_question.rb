5bdcdcbfb76086e5fae0de337a94154dccf6f02c
vanderhoop <vanderhoop@me.com>
2014-12-23 10:54:29 -0600

    semanticizes DataStructureAccessQuestion

--- lib/battleroom/models/data_structure_access_question.rb
+++ lib/battleroom/models/data_structure_access_question.rb
@@ -7,7 +7,7 @@ class DataStructureAccessQuestion < DataStructureQuestion
     answer_value_string = format_value_for_stdout_and_eval(answer_value)
     battleprint "Given the data structure below, access the #{answer_value_class} value ".blue + answer_value_string.yellow + ".\n".blue
     print "#{variable_name} = ".green
-    if data_structure.to_s.length < 100
+    if data_structure.to_s.length < 45
       print_data_structure_on_single_line
     else
       ap(data_structure, { indent: -2, index: false, multiline: true, plain: true })
@@ -28,11 +28,19 @@ class DataStructureAccessQuestion < DataStructureQuestion
     battleprint(single_line_hash_as_string)
   end
 
+  def eval_returns_the_correct_value?(user_input)
+    evaluation_scope.eval(user_input) == answer_value
+  end
+
+  def user_input_uses_variable?(user_input)
+    user_input.include?(variable_name)
+  end
+
   def evaluate_data_structure_access_input
     enter_evaluation_loop do |user_submission|
       begin
         provide_evaluation_scope_with_variable_assignment_necessary_for_answer_eval()
-        if evaluation_scope.eval(user_submission) == answer_value && user_submission.include?(variable_name)
+        if eval_returns_the_correct_value?(user_submission) && user_input_uses_variable?(user_submission)
           true
         else
           battleprint "Remember, #{hint} Try again.".red
